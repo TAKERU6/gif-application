e52eb1e03fc548e7b4b58f8519bf38579a7d6d1d
admim <admim@admims-MBP.fritz.box>
2020-11-14 21:21:09 +0100

    add redux-thunk
    install redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import reducers from "./reducers";
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunk));
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
